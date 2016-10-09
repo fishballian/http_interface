a288e690b00d5e09447bd6d0edccb794c8afb35c
yuanxiaopeng <fishballian@qq.com>
2016-10-09 09:38:14 +0800

    init

---
+++ src/http_interface_sup.erl
@@ -0,0 +1,40 @@
+%%%-------------------------------------------------------------------
+%% @doc http_interface top level supervisor.
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(http_interface_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%====================================================================
+%% Supervisor callbacks
+%%====================================================================
+
+%% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
+init([]) ->
+    Child = {gen_connection_sup, 
+             {gws_connection_sup, 
+              start_link, 
+              [hi_server, {0, 0, 0, 0}, 1156, []]}, 
+             transient, 5000, supervisor, [gws_connection_sup]},   
+    {ok, { {one_for_all, 0, 1}, [Child]} }.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
